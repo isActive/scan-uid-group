844a33d111ecab1002779086743ed88e95854ec7
MinHien <ghosnigh33@gmail.com>
2023-04-14 18:08:20 +0700

    Add files via upload

---
+++ FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanUIDMemberGroup
+{
+    public class FileHelper
+    {
+        public static void SaveSuccess(string data,string idGroup)
+        {
+            string pathSave = AppDomain.CurrentDomain.BaseDirectory + $"\\success_{idGroup}.txt";
+            var filetoappend = new FileInfo(pathSave);
+            using (var sw = filetoappend.AppendText())
+            {
+                sw.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ScanUIDMemberGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "SCAN UID MEMBERS GROUP BY MINH HIEN";
+            Crawl();
+
+            Console.ReadLine();
+        }
+        static void Crawl()
+        {
+            REBACK:
+            int checkMem = 0;
+            string cookie = "";
+            do
+            {
+                Console.Write("INPUT COOKIE: ");
+                cookie = Console.ReadLine();
+
+            } while (!cookie.Contains("c_user") || string.IsNullOrEmpty(cookie));
+            Console.Clear();
+            Console.Write("INPUT UID GROUP: ");
+            var idGroup = Console.ReadLine();
+            Console.Write("INPUT AMOUNT: ");
+            var amount = Console.ReadLine();
+            Request request = new Request(cookie);
+            string resGroup = request.RequestGet($"https://mbasic.facebook.com/browse/group/members/?id={idGroup}&start=0&listType=list_nonfriend_nonadmin");
+            if(Regex.Match(resGroup, "<title>(.*?)</title>").Groups[1].Value.Contains("Đăng nhập"))
+            {
+                Console.Clear();
+                Console.WriteLine("ĐĂNG NHẬP THẤT BẠI! ADD LẠI COOKIE");
+                goto REBACK;
+            }
+            Console.Clear();
+        ReGet:
+            MatchCollection matches = Regex.Matches(resGroup, "id=\"member_(.*?)\"");
+            string iMore = Regex.Match(resGroup, "id=\"m_more_item\"><a href=\"(.*?)\"").Groups[1].Value.Replace("&amp;","&");
+            Console.Title = $"SCAN UID MEMBERS GROUP BY MINH HIEN - RUNNING GROUP {idGroup}";
+            foreach (Match id in matches)
+            {
+                string pathID = Regex.Match(id.Value.ToString(), "member_(.*?)\"").Groups[1].Value;
+                if (checkMem == Int32.Parse(amount))
+                {
+                    Console.WriteLine("Crawl DONE");
+                    return;
+                } // Check Amount
+                Console.WriteLine($"[{checkMem++}] - {pathID}");
+                FileHelper.SaveSuccess(pathID, idGroup);
+            }
+            resGroup = request.RequestGet($"https://mbasic.facebook.com{iMore}");
+            goto ReGet;
+        }
+    }
+}
